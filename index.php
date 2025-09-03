71c8c810752a06e39f32bd7f544cef9c1fec23ec
VIGNESHWARAN1492 <39028514+VIGNESHWARAN1492@users.noreply.github.com>
2025-09-03 15:21:41 +0530

    Delete index.php

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "<h1>PHP App is Running Successfully on Azure!</h1>";
-echo "<p>Current Date and Time: " . date("Y-m-d H:i:s") . "</p>";
-phpinfo(); // Displays PHP configuration info
-?>
